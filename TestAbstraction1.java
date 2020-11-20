de1d4a4586620e64be2ce6238d4dc0115cf72f11
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-11-20 15:02:46 +0530

    Update TestAbstraction1.java

--- TestAbstraction1.java
+++ TestAbstraction1.java
@@ -1,12 +1,12 @@
  abstract class Bike {  
    Bike () { 
     System.out.println ("bike is created");
-    }
+           }
    abstract void run ();  
    void changeGear () { 
     System.out.println ("gear changed"); 
+                 }  
    }  
- }  
  class Honda extends Bike {  
  void run () {
   System.out.println ("running safely.."); 
